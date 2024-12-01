c9f2dd663021ea0264cd602a4edf9b7d0111d53a
Salma <46s1943@utas.edu.om>
2024-12-01 06:15:55 +0400

    cnage footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>Jasmin.Estudillo@utas.edu.om|©2024|PostIT|All Rights Reserved.</div>
+      <div>salma@utas.edu.om|©2024|PostIT|All Rights Reserved.</div>
     </footer>
   );
 };
