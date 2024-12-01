efd52165810502114909f61055df8b8d23a45e21
Salma <46s1943@utas.edu.om>
2024-12-01 06:21:32 +0400

    cnange the footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>sara@utas.edu.om|©2024|PostIT|All Rights Reserved.</div>
+      <div>salma@utas.edu.om|©2024|PostIT|All Rights Reserved.</div>
     </footer>
   );
 };
